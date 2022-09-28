eaa6b3798d05a35299ac7d0eb3c9edb668570257
Dong-Jae Oh <93889207+djDongjae@users.noreply.github.com>
2022-09-28 22:30:36 +0900

    프로젝트 생성

--- src/main/java/com/peenector/server/web/dto/HelloResponseDto.java
+++ src/main/java/com/peenector/server/web/dto/HelloResponseDto.java
@@ -1,2 +1,13 @@
-package com.peenector.server.web.dto;public class HelloResponseDto {
+package com.peenector.server.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
 }
